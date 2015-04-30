dd203824074a3f1af305cca8d6d6a59e3a442d66
Frederick Gnodtke <frederick@gnodtke.net>
2015-04-30 18:33:01 +0200

    ESpeak now working.

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 var Mumble = require("mumble");
 var ESpeak = require("node-espeak");
 var Samplerate = require("samplerate");
+var FS = require("fs");
 
 var options = require("./config");
 
 var connection;
+var is;
 
 function startup(_connection) {
 	connection = _connection;
 	connection.authenticate(options.name);
 	connection.on("initialized", function() {
+		is = connection.inputStream();
 		ESpeak.speak("Initialized!");
 		setInterval(function() {
-			ESpeak.speak("Hello, World. I love you!");
+			ESpeak.speak("Lorem Ipsum Dolor Sit Amet!");
 		}, 2000);
+		FS.createReadStream("out.pcm").pipe(connection.inputStream());
 	});
 	connection.on("textMessage", function(data) {
-		ESpeak.speak(data.message);
+		//ESpeak.speak(data.message);
 	});
 }
 
 
+var outs = FS.createWriteStream("out.pcm");
+var ins = FS.createWriteStream("in.pcm");
+
 ESpeak.initialize();
 ESpeak.onVoice(function(wav, samples, samplerate) {
-	connection.inputStream().write(Samplerate.resample(wav, samplerate, 48000, 2, samples));
-	console.log("Samples: " + samples);
-	console.log("Samplerate: " + samplerate);
+	var resampled = Samplerate.resample(wav, samplerate, 48000, 1);
+
+	ins.write(wav);
+	outs.write(resampled);
+
+	is.write(resampled);
+
+	console.log("  Samples: " + samples);
+	console.log("  Samplerate: " + samplerate);
+	console.log("  BufferLenght after conversion: " + resampled.length);
+	console.log("------");
 });
 
 Mumble.connect("mumble://" + options.url, options.mumbleOptions, function(err, connection) {
