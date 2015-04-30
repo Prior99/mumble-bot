ac6121244dd92296fc8287b399a8b2b3aa83b3dd
Frederick Gnodtke <frederick@gnodtke.net>
2015-04-30 15:12:28 +0200

    Started adding samplerate.

---
+++ index.js
@@ -0,0 +1,38 @@
+var Mumble = require("mumble");
+var ESpeak = require("node-espeak");
+var Samplerate = require("samplerate");
+
+var options = require("./config");
+
+var connection;
+
+function startup(_connection) {
+	connection = _connection;
+	connection.authenticate(options.name);
+	connection.on("initialized", function() {
+		ESpeak.speak("Initialized!");
+		setInterval(function() {
+			ESpeak.speak("Hello, World. I love you!");
+		}, 2000);
+	});
+	connection.on("textMessage", function(data) {
+		ESpeak.speak(data.message);
+	});
+}
+
+
+ESpeak.initialize();
+ESpeak.onVoice(function(wav, samples, samplerate) {
+	connection.inputStream().write(Samplerate.resample(wav, samplerate, 48000, 1, samples));
+	console.log("Samples: " + samples);
+	console.log("Samplerate: " + samplerate);
+});
+
+Mumble.connect("mumble://" + options.url, options.mumbleOptions, function(err, connection) {
+	if(err) {
+		throw err;
+	}
+	else {
+		startup(connection);
+	}
+});
