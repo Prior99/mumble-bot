734f7ec6830050c153eed0a3457e5d0b27f1b64b
Frederick Gnodtke <frederick@gnodtke.net>
2015-05-12 07:52:29 +0200

    Added playlist

--- src/website/music/playlist.js
+++ src/website/music/playlist.js
@@ -1,8 +1,13 @@
+var bot;
+
 function render(req, res) {
+	var playlist = bot.mpd.mpd.playlist;
 	res.render('music/playlist', {
+		playlist: playlist
 	});
 }
 
-module.exports = function() {
+module.exports = function(b) {
+	bot = b;
 	return render;
 };
