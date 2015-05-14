50756e3b20bb1b288e54400bd32a8236c44373d0
Frederick Gnodtke <frederick@gnodtke.net>
2015-05-14 13:15:46 +0200

    Update songs before printing list

--- src/website/api/music/songs.js
+++ src/website/api/music/songs.js
@@ -1,10 +1,12 @@
 module.exports = function(bot) {
 	return function(req, res) {
-		var arr = [];
-		var songs = bot.mpd.mpd.songs;
-		for(var i in bot.mpd.mpd.songs) {
-			arr.push(songs[i].flatCopy());
-		}
-		res.send(JSON.stringify(arr));
+		bot.mpd.mpd._updateSongs(function() {
+			var arr = [];
+			var songs = bot.mpd.mpd.songs;
+			for(var i in bot.mpd.mpd.songs) {
+				arr.push(songs[i].flatCopy());
+			}
+			res.send(JSON.stringify(arr));
+		});
 	}
 };
