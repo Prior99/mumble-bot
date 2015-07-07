f2792063fcef4f0152fac737f82ddbf265bc77bd
Frederick Gnodtke <frederick@gnodtke.net>
2015-07-07 16:33:36 +0200

    Added jsdoc to quotes.js

--- src/quotes.js
+++ src/quotes.js
@@ -5,6 +5,13 @@ var Winston = require('winston');
 /*
  * Code
  */
+
+/**
+ * Provides a more nice to use interface for handling quotes than operating
+ * directly on the database.
+ * @constructor
+ * @param bot - Bot this instance is connected to.
+ */
 var Quotes = function(bot) {
 	this.bot = bot;
 	bot.newCommand("speak", function() {
@@ -12,6 +19,13 @@ var Quotes = function(bot) {
 	}.bind(this), "Gibt einen zufälligen intelligenten Kommentar aus der Datenbank aus.", "quote-left");
 };
 
+/**
+ * Speak a quote. If the id is defined, a specific quote will be spoken. If it
+ * is undefined, a random quote will be chosen from the database.
+ * @param {number} id - Id of the quote to speak. If this is undefined a random
+ * 						quote will be chosen.
+ * @param callback - Will be called once the quote was spoken.
+ */
 Quotes.prototype.speak = function(id, callback) {
 	if(id === undefined) {
 		this.speakRandom(callback);
@@ -28,14 +42,28 @@ Quotes.prototype.speak = function(id, callback) {
 	}
 };
 
+/**
+ * Add a quote to the database.
+ * @param {string} quote - Text of the quote.
+ * @param {string} author - Author of the quote.
+ * @param callback - `Called once the quote was submitted.
+ */
 Quotes.prototype.add = function(quote, author, callback) {
 	this.bot.database.addQuote(quote, author, callback);
 };
 
+/**
+ * Retrieve an array of all known quotes.
+ * @param callback - Called once the array was retrieved.
+ */
 Quotes.prototype.list = function(callback) {
 	this.bot.database.getQuoteList(callback);
 };
 
+/**
+ * Counts all quotes in the database.
+ * @param callback - Called once the quotes were counted.
+ */
 Quotes.prototype.count = function(callback) {
 	this.bot.database.getQuoteCount(callback);
 };
@@ -44,6 +72,10 @@ Quotes.prototype._dispatch = function(quote, callback) {
 	this.bot.say(quote.quote, callback);
 };
 
+/**
+ * Speaks a randomly chosen quote from the database.
+ * @param callback - Called once the quote was spoken.
+ */
 Quotes.prototype.speakRandom = function(callback) {
 	this.bot.database.getRandomQuote(function(err, quote) {
 		if(err) {
