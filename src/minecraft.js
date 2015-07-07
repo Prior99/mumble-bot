cd6c275beda9a43eaba4c54e179d5bfbf7a4386a
Frederick Gnodtke <frederick@gnodtke.net>
2015-07-07 15:37:24 +0200

    Added jsdoc to minecraft.js

--- src/minecraft.js
+++ src/minecraft.js
@@ -1,6 +1,6 @@
 
 /*
- * Incluports
+ * Includes
  */
 var Mineflayer = require('mineflayer');
 var Winston = require('winston');
@@ -8,46 +8,59 @@ var EventEmitter = require("events").EventEmitter;
 var Util = require("util");
 
 /*
- * Zeh-oh-de-eh
+ * Code
  */
 
-var Minecraft = function(options, bot, callback) {
-    this.bot = bot;
-    Winston.info("Connecting to Minecraft server on " + options.host + ":" + options.port + " as " + bot.options.name + " ...");
-    this.mc = Mineflayer.createBot({
-        host : options.host,
-        port : options.port,
-        username : bot.options.name,
-        keepAlive : true
-    });
-    this._ready = false;
-    this.mc.once('spawn', function() {
-        Winston.info("Connected to minecraft and spawned at " + this.mc.entity.position);
-        this._ready = true;
-    }.bind(this));
-    this.mc.on('chat', this._onChat.bind(this));
+/**
+ * This  class handles the connection of the bot to minecraft.
+ * @constructor
+ * @param options - Options for the connection read from config file
+ * @param bot - The bot this minecraft class is attached to
+ */
+var Minecraft = function(options, bot) {
+	this.bot = bot;
+	Winston.info("Connecting to Minecraft server on " + options.host + ":" + options.port + " as " + bot.options.name + " ...");
+	this.mc = Mineflayer.createBot({
+		host : options.host,
+		port : options.port,
+		username : bot.options.name,
+		keepAlive : true
+	});
+	this._ready = false;
+	this.mc.once('spawn', function() {
+		Winston.info("Connected to minecraft and spawned at " + this.mc.entity.position);
+		this._ready = true;
+	}.bind(this));
+	this.mc.on('chat', this._onChat.bind(this));
 };
 
 Util.inherits(Minecraft, EventEmitter);
 
 Minecraft.prototype._onChat = function(username, message) {
-    if(username !== this.mc.username) {
-        this.bot.command.processPrefixed(message);
-    }
+	if(username !== this.mc.username) {
+		this.bot.command.processPrefixed(message);
+	}
 };
 
+/**
+ * Say something in the minecraft servers chat.
+ * @param msg - The message to say in the servers chat
+ */
 Minecraft.prototype.say = function(msg) {
-    if(this._ready) {
-        this.mc.chat(msg);
-    }
+	if(this._ready) {
+		this.mc.chat(msg);
+	}
 };
 
+/**
+ * Disconnect from minecraft gently and shutdown this instance.
+ */
 Minecraft.prototype.stop = function() {
-    Winston.info("Closing connection to Minecraft ...");
-    this.mc.on('end', function() {
-        Winston.info("Disconnected from Minecraft.");
-    });
-    this.mc.quit();
+	Winston.info("Closing connection to Minecraft ...");
+	this.mc.on('end', function() {
+		Winston.info("Disconnected from Minecraft.");
+	});
+	this.mc.quit();
 };
 
 module.exports = Minecraft;
