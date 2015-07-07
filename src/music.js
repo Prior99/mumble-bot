2c6b8f2470c1978fb3affddbee8db975bc315905
Frederick Gnodtke <frederick@gnodtke.net>
2015-07-07 16:26:48 +0200

    Added jsdoc to music.js

--- src/music.js
+++ src/music.js
@@ -6,6 +6,15 @@ var Winston = require('winston');
 /*
  * Code
  */
+
+/**
+ * Handles playback of music from a fifo fed by a music player daemon.
+ * This only echos the music streamed from the fifo into the mumble server and
+ * does not control the playback, which is handled by mpdcontrol.js
+ * @constructor
+ * @param bot - Bot this instance is attached to.
+ */
+
 var Music = function(bot) {
 	this.fifo = FS.createReadStream(bot.options.mpd.fifo);
 	this.inputStream = bot.mumble.inputStream();
@@ -20,20 +29,36 @@ var Music = function(bot) {
 	Winston.info("Module started: Music");
 };
 
+/**
+ * Not working. This method should scale a pcm signal to lower or increase the
+ * volume.
+ * @param buffer - Buffer containing the pcm data to be scaled
+ * @param {number} volume - Factor to scale the pcm data by.
+ */
 function scaleVolume(buffer, volume) {
 	for(var i = 0; i < buffer.length; i++) {
 		buffer[i] *= volume;
 	}
 };
 
+/**
+ * Toggles muting of playback. If the playback is muted, it will be unmuted and
+ * vice-verse.
+ */
 Music.prototype.toggleMute = function() {
 	this.muted = !this.muted;
 };
 
+/**
+ * Mutes the playback.
+ */
 Music.prototype.mute = function() {
 	this.muted = true;
 };
 
+/**
+ * Unmutes the playback.
+ */
 Music.prototype.unmute = function() {
 	this.muted = false;
 };
