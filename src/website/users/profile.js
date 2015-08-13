af0927e5a24dc9a69cb5eacb484b7452746d12ce
Frederick Gnodtke <frederick@gnodtke.net>
2015-08-13 09:39:35 +0200

    Moved linking mumble users from profile to settings

--- src/website/users/profile.js
+++ src/website/users/profile.js
@@ -1,50 +1,11 @@
 var Winston = require('winston');
 
 /**
- * Returns only those users which have a unique id and are thous registered on
- * the mumble server.
- */
-function getRegisteredMumbleUsers(mumble) {
-	var users = mumble.users();
-	var result = [];
-	for(var i in users) {
-		if(users[i].id) {
-			result.push(users[i]);
-		}
-	}
-	return result;
-}
-
-/**
  * <b>/users/profile/:username</b> Display the profile of a specific user.
  * @param {Bot} bot - Bot the webpage belongs to.
  */
 var ViewUsersProfile = function(bot) {
-	function getMumbleUsersLinkingPossible(mumbleUsers, cb) {
-		var arr = [];
-		bot.database.getLinkedMumbleUsers(function(err, mumbleIds) {
-			if(err) {
-				Winston.error("Error fetching registered mumble users", err);
-				cb([]);
-			}
-			else {
-				for(var i in mumbleUsers) {
-					var u = mumbleUsers[i];
-					var linked = false;
-					for(var j in mumbleIds) {
-						if(mumbleIds[j].id == u.id) {
-							linked = true;
-							break;
-						}
-					}
-					if(!linked && u.id != bot.mumble.user.id) {
-						arr.push(u);
-					}
-				}
-				cb(arr);
-			}
-		});
-	}
+
 
 	function fetchUser(username, req, res) {
 		bot.database.getUserByUsername(username, function(err, user) {
@@ -84,14 +45,12 @@ var ViewUsersProfile = function(bot) {
 	}
 
 	function renderPage(linkedUsers, records, user, username, req, res) {
-		getMumbleUsersLinkingPossible(getRegisteredMumbleUsers(bot.mumble), function(mumbleUsers) {
-			res.locals.user = user;
-			res.locals.freeMumbleUsers = mumbleUsers;
-			res.locals.linkedUsers = linkedUsers;
-			res.locals.own = req.session.user.id == user.id;
-			res.locals.records = records;
-			res.render("users/profile");
-		});
+		res.locals.user = user;
+
+		res.locals.linkedUsers = linkedUsers.map(function(user) { return bot.mumble.userById(user.id); });
+		res.locals.own = req.session.user.id == user.id;
+		res.locals.records = records;
+		res.render("users/profile");
 	}
 
 	return function(req, res) {
--- src/website/users/settings.js
+++ src/website/users/settings.js
@@ -2,6 +2,33 @@ var Winston = require('winston');
 
 
 var ViewSettings = function(bot) {
+
+	function getMumbleUsersLinkingPossible(mumbleUsers, cb) {
+		var arr = [];
+		bot.database.getLinkedMumbleUsers(function(err, mumbleIds) {
+			if(err) {
+				Winston.error("Error fetching registered mumble users", err);
+				cb([]);
+			}
+			else {
+				for(var i in mumbleUsers) {
+					var u = mumbleUsers[i];
+					var linked = false;
+					for(var j in mumbleIds) {
+						if(mumbleIds[j].id == u.id) {
+							linked = true;
+							break;
+						}
+					}
+					if(!linked && u.id != bot.mumble.user.id) {
+						arr.push(u);
+					}
+				}
+				cb(arr);
+			}
+		});
+	}
+
 	return function(req, res) {
 		var user = req.session.user;
 		bot.database.getUserByUsername(user.username, function(err, user) { //Reload user from database
@@ -11,8 +38,18 @@ var ViewSettings = function(bot) {
 			}
 			else {
 				if(user) {
-					res.locals.user = user;
-					res.render("users/settings")
+					getMumbleUsersLinkingPossible(bot.getRegisteredMumbleUsers(), function(mumbleUsers) {
+						bot.database.getLinkedMumbleUsersOfUser(user.username, function(err, linkedUsers) {
+							if(err) {
+								Winston.error("Unabled to fetch linked mumble users of user " + user.username, err);
+								linkedUsers = [];
+							}
+							res.locals.user = user;
+							res.locals.linkedUsers = linkedUsers.map(function(user) { return bot.mumble.userById(user.id); });
+							res.locals.freeMumbleUsers = mumbleUsers;
+							res.render("users/settings");
+						});
+					});
 				}
 				else {
 					res.status(404).send("Unknown user.");
