02e69251c3a9bf384efa780b9d178dd6236b4d5f
Frederick Gnodtke <frederick@gnodtke.net>
2015-08-13 10:43:15 +0200

    Removed useless verbosity.

--- src/website/queue.js
+++ src/website/queue.js
@@ -2,7 +2,6 @@ var Winston = require('winston');
 
 var ViewQueue = function(bot) {
 	return function(req, res) {
-		console.log(bot.output.queue);
 		res.locals.queue = bot.output.queue;
 		res.render('queue');
 	};
