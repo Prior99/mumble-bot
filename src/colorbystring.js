f442ac1025eb1b6daa082e8f103eeb5f7340c95c
Frederick Gnodtke <frederick@gnodtke.net>
2015-10-24 23:04:26 +0200

    Tweaked style of labels.

--- src/colorbystring.js
+++ src/colorbystring.js
@@ -4,8 +4,8 @@ var Color = require('onecolor');
 module.exports = function(string) {
 	var rng = new RNG(string);
 	var hue = rng.uniform();
-	var saturation = rng.uniform() / 3 + 0.6666;
-	var brightness = rng.uniform() / 2;
+	var saturation = 1;
+	var brightness = 0.4;
 	var color = Color("#FFFFFF").hue(hue).saturation(saturation).lightness(brightness).hex();
 	return color;
 };
