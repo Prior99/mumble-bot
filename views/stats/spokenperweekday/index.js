16c9ef00bdd80fa1f04c7dd4b2da5262f81adc01
Frederick Gnodtke <frederick@gnodtke.net>
2015-11-26 02:35:14 +0100

    More cool statistics for weekday.

--- views/stats/spokenperweekday/index.js
+++ views/stats/spokenperweekday/index.js
@@ -2,70 +2,52 @@
 	var d3 = require("d3");
 	var $ = require("jquery");
 
-	var margin = {
-		top: 20,
-		right: 20,
-		bottom: 80,
-		left: 50
-	};
-	var width = $("#chart").width() - margin.left - margin.right;
-	var height = $("#chart").height() - margin.top - margin.bottom;
-	var x = d3.scale.ordinal()
-		.rangeRoundBands([0, width], .1);
-	var y = d3.time.scale()
-		.range([height, 0]);
-	var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
-
-	var yAxis = d3.svg.axis()
-		.scale(y)
-		.orient("left")
-		.ticks(d3.time.hours, 2)
-		.tickFormat(function(d) {
-			return Math.round((d / (1000 * 60 * 60)) * 100) / 100;
+	var width = $("#chart").width();
+	var height = $("#chart").height();
+	var maxRadius = Math.min(width, height) / 2;
+	var pie = d3.layout.pie()
+		.sort(null)
+		.value(function(d) {
+			return 1;
 		});
-	var chart = d3.select("#chart")
+	var perUserPie = d3.select("#chart")
 		.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+		.attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")");
 	d3.json('/api/stats/spokenperweekday', function(err, data) {
 		if(err) {
 			throw err;
 		}
-		x.domain(data.map(function(d) {
-			return d.day;
-		}));
-		y.domain([0, d3.max(data, function(d) {
+		var maxDate = d3.max(data, function(d) {
 			return new Date(d.amount);
-		})]);
-		chart.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-			.call(xAxis);
+		}).getTime();
+		for(var i = 0; i < 7; i++) {
+			data[i].num = i;
+			data[i].amount = new Date(data[i].amount).getTime();
+		}
+		var d = perUserPie.append("g")
+			.attr("class", "dat");
+		var g = d.selectAll(".arc")
+			.data(pie(data))
+			.enter().append("g")
+			.attr("class", "arc");
 
-		chart.append("g")
-			.attr("class", "y axis")
-			.call(yAxis)
-			.append("text");
-
-		var bar = chart.selectAll(".bar")
-			.data(data)
-			.enter()
-			.append("g")
-				.attr("class", "dat");
-		bar.append("rect")
-			.attr("class", "bar")
-			.attr("x", function(d) {
-				return x(d.day);
+		g.append("path")
+			.attr("d", d3.svg.arc()
+				.outerRadius(function(d, i) {
+					return (d.data.amount/maxDate) * maxRadius * 0.7 + maxRadius * 0.3;
+				})
+				.innerRadius(maxRadius * 0.3))
+			.style("fill", "#3182BD");
+		g.append("text")
+			.attr("transform", function(d, i) {
+				d.innerRadius = 0.2 * maxRadius;
+				d.outerRadius = 0.3 * maxRadius;
+				return "translate(" + d3.svg.arc().centroid(d) + "), rotate(" + (360 * (d.data.num / 7) + (360 / 7) / 2) + ")";
 			})
-			.attr("width", x.rangeBand())
-			.attr("y", function(d) {
-				return y(new Date(d.amount));
-			})
-			.attr("height", function(d) {
-				return height - y(new Date(d.amount));
-			})
-			.style("fill", "#FD8D3C");
+			.style("fill", "black")
+			.attr("text-anchor", "middle")
+			.text(function(d, i) {
+				return d.data.day;
+			});
 	});
 })();
