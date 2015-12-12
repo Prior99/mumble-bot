82cb317b84057bb12b2d835809ffbf3ed83a76ac
Sascha <alexander.dmitriev@tu-harburg.de>
2015-12-12 21:08:48 +0100

    partial database lint

--- src/database/mumbleUsers.js
+++ src/database/mumbleUsers.js
@@ -1,18 +1,19 @@
-module.exports = function(Database) {
+class Database {
 	/**
 	 * Will return an array containing all mumble user ids of mumble users which
 	 * are linked to users in this bot and their counterparts usernames.
 	 * @param callback - Called when the query is done.
 	 */
-	Database.prototype.getLinkedMumbleUsers = function(callback) {
-		this.pool.query("SELECT m.mumbleId AS id, u.username AS username FROM MumbleUsers m LEFT JOIN Users u ON u.id = m.user",
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					callback(null, rows);
+	getLinkedMumbleUsers(callback) {
+		this.pool.query("SELECT m.mumbleId AS id, u.username AS username"
+			+ " FROM MumbleUsers m LEFT JOIN Users u ON u.id = m.user",
+				(err, rows) => {
+					if(this._checkError(err, callback)) {
+						callback(null, rows);
+					}
 				}
-			}.bind(this)
-		);
-	};
+		)
+	}
 
 	/**
 	 * Returns all mumble user ids of mumble users which are linked to the
@@ -21,15 +22,17 @@ module.exports = function(Database) {
 	 *							  fetched.
 	 * @param callback - Called once the query is done.
 	 */
-	Database.prototype.getLinkedMumbleUsersOfUser = function(username, callback) {
-		this.pool.query("SELECT m.mumbleId AS id FROM MumbleUsers m LEFT JOIN Users u ON u.id = m.user WHERE u.username = ?",
-			[username], function(err, rows) {
+	getLinkedMumbleUsersOfUser(username, callback) {
+		this.pool.query(
+			"SELECT m.mumbleId AS id FROM MumbleUsers m LEFT JOIN Users u ON u.id = m.user WHERE u.username = ?",
+			[username],
+			(err, rows) => {
 				if(this._checkError(err, callback)) {
 					callback(null, rows);
 				}
-			}.bind(this)
-		);
-	};
+			}
+		)
+	}
 
 	/**
 	 * Links a mumble user to a user in this bot.
@@ -38,23 +41,25 @@ module.exports = function(Database) {
 	 *							  linked.
 	 * @param callback - Called when the query has been finished.
 	 */
-	Database.prototype.linkMumbleUser = function(id, username, callback) {
-		this.pool.query("INSERT INTO MumbleUsers(mumbleId, user) VALUES(?, (SELECT id FROM Users WHERE username = ?))",
-			[id, username], function(err, rows) {
+	linkMumbleUser(id, username, callback) {
+		this.pool.query(
+			"INSERT INTO MumbleUsers(mumbleId, user) VALUES(?, (SELECT id FROM Users WHERE username = ?))",
+			[id, username],
+			(err, rows) => {
 				if(this._checkError(err, callback)) {
 					callback(null);
 				}
-			}.bind(this)
-		);
-	};
+			}
+		)
+	}
 
 	/**
 	 * Returns the full user to which a mumble user id is linked.
 	 * @param {number} id - Id of the mumble user to check.
 	 * @param callback - Called once the query is done.
 	 */
-	Database.prototype.getLinkedUser = function(id, callback) {
-		this.pool.query("SELECT user FROM MumbleUsers WHERE mumbleId = ?", [id], function(err, rows) {
+	getLinkedUser(id, callback) {
+		this.pool.query("SELECT user FROM MumbleUsers WHERE mumbleId = ?", [id], (err, rows) => {
 			if(this._checkError(err, callback)) {
 				if(rows.length > 0) {
 					this.getUserById(rows[0].user, callback);
@@ -63,6 +68,8 @@ module.exports = function(Database) {
 					callback(null, null);
 				}
 			}
-		}.bind(this));
-	};
-};
+		});
+	}
+}
+
+module.exports = Database;
