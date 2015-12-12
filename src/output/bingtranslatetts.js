caaa39c2a7aa53d97d045304963a0e0fe5027dae
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-12 21:59:55 +0100

    All three child classes of cached webbtts (google, bing and responsive voice) now lint free.

--- src/output/bingtranslatetts.js
+++ src/output/bingtranslatetts.js
@@ -2,68 +2,84 @@
  * Imports
  */
 
-var CachedWebTTS = require('./cachedwebtts');
-var Request = require("request");
-var Winston = require("winston");
+import * as CachedWebTTS from "./cachedwebtts";
+import * as Request from "request";
+import * as Winston from "winston";
 
 /*
  * Code
  */
 
-function getAccessToken(clientID, clientSecret, callback) {
+/**
+ * Refresh the access token for Bing TTS.
+ * @param {string} clientID - The client id with which to login.
+ * @param {string} clientSecret - The secret to use for authenticating.
+ * @param {callback} callback - Called when the token was refreshed with the parsed body as first argument.
+ * @return {undefined}
+ */
+const getAccessToken = function(clientID, clientSecret, callback) {
 	//clientID = encodeURIComponent(clientID);
 	//clientSecret = encodeURIComponent(clientSecret);
 	Request.post({
 		url : "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13",
 		form : {
-			'grant_type' : 'client_credentials',
-			'client_id' : clientID,
-			'client_secret' : clientSecret,
-			'scope' : 'http://api.microsofttranslator.com'
+			"grant_type" : "client_credentials",
+			"client_id" : clientID,
+			"client_secret" : clientSecret,
+			"scope" : "http://api.microsofttranslator.com"
 		}
-	}, function(err, http, body){
-		callback(JSON.parse(body));
-	});
+	}, (err, http, body) => callback(JSON.parse(body)));
 }
 
-module.exports = function(clientID, clientSecret, database) {
-	var tts = new CachedWebTTS({
+/**
+ * The Bing TTS text to speech api as cached web tts.
+ * @param {string} clientID - The client id with which to login.
+ * @param {string} clientSecret - The secret to use for authenticating.
+ * @param {Database} database - Database to write the cache entries to.
+ * @return {undefined}
+ */
+const BingTTS = function(clientID, clientSecret, database) {
+	const tts = new CachedWebTTS({
 		url : "",
 		cacheDir : "bing-tts-cache",
 		//splitAfter : 90,
 		header : { },
 		storeCallback : function(text, callback) {
-			database.addCachedTTS('bing_' + tts.gender, text, callback);
+			database.addCachedTTS("bing_" + tts.gender, text, callback);
 		}.bind(tts),
 		retrieveCallback : function(text, callback) {
-			database.getCachedTTS('bing_' + tts.gender, text, callback);
+			database.getCachedTTS("bing_" + tts.gender, text, callback);
 		}.bind(tts)
 	});
 
 	tts.baseUrl = "http://api.microsofttranslator.com/v2/Http.svc/Speak?language=de&format=audio/mp3";
-
 	tts.setGender = function(gender) {
 		tts.gender = gender;
 		tts.resetURL();
 	};
-
 	tts.setAccessToken = function(token) {
 		tts.accessToken = token;
 		tts.resetURL();
 	};
-
 	tts.resetURL = function() {
-		tts.url = tts.baseUrl + "&options=MaxQuality|" + tts.gender + "&appId=Bearer%20" + encodeURIComponent(tts.accessToken) + "&text="
+		tts.url = tts.baseUrl +
+			"&options=MaxQuality|" + tts.gender +
+			"&appId=Bearer%20" + encodeURIComponent(tts.accessToken) + "&text="
 	};
 
-	function refreshAccessToken() {
+	const refreshAccessToken = function() {
 		Winston.info("Refreshing access token for bing tts...");
-		getAccessToken(clientID, clientSecret, function(response) {
-			tts.header['Authorization'] = "Bearer " + response['access_token'];
-			tts.setAccessToken(response['access_token']);
+		getAccessToken(clientID, clientSecret, (response) => {
+			tts.header["Authorization"] = "Bearer " + response["access_token"];
+			tts.setAccessToken(response["access_token"]);
 		});
 	}
 	refreshAccessToken();
-	setInterval(refreshAccessToken, 1000 * 60 * 9); //Refresh every 9 minutes
+	const millisecondsPerSecond = 1000;
+	const secondsPerMinute = 60;
+	const nineMinutes = millisecondsPerSecond * secondsPerMinute * 9;
+	setInterval(refreshAccessToken, nineMinutes); //Refresh every 9 minutes
 	return tts;
 };
+
+export default BingTTS;
--- src/output/responsivetts.js
+++ src/output/responsivetts.js
@@ -2,26 +2,33 @@
  * Imports
  */
 
-var CachedWebTTS = require('./cachedwebtts');
+import * as CachedWebTTS from "./cachedwebtts";
 /*
  * Code
  */
 
-module.exports = function(database) {
+/**
+ * The Google Translate TTS text to speech api as cached web tts.
+ * @param {Database} database - Database to write the cache entries to.
+ * @return {undefined}
+ */
+const ResponsiveVoiceTTS = function(database) {
 	return new CachedWebTTS({
 		url : "https://code.responsivevoice.org/getvoice.php?tl=de&sv=&vn=&pitch=0.5&rate=0.5&vol=1&t=",
 		cacheDir : "rsponsive-tts-cache",
 		splitAfter : 90,
 		header : {
-			'Host' : "code.responsivevoice.org",
-			'Referer' : "http://responsivevoice.org/",
-			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0'
+			"Host" : "code.responsivevoice.org",
+			"Referer" : "http://responsivevoice.org/",
+			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0"
 		},
-		storeCallback : function(text, callback) {
-			database.addCachedTTS('responsive', text, callback);
+		storeCallback(text, callback) {
+			database.addCachedTTS("responsive", text, callback);
 		},
-		retrieveCallback : function(text, callback) {
-			database.getCachedTTS('responsive', text, callback);
+		retrieveCallback(text, callback) {
+			database.getCachedTTS("responsive", text, callback);
 		}
 	});
 };
+
+export default ResponsiveVoiceTTS;
