b8f4d0ea67dcc9c65fab2cca68c11f5de410be97
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-12 20:42:25 +0100

    src/website/api/bass/ lint free.

--- src/website/api/bass/addeffect.js
+++ src/website/api/bass/addeffect.js
@@ -1,31 +1,41 @@
-var Winston = require('winston');
+import * as Winston from "winston";
+import * as HTTPCodes from "../../httpcodes";
 
-module.exports = function(bot) {
+/**
+ * View for displaying all effects.
+ * @param {Bot} bot - Bot the webpage belongs to.
+ * @return {ViewRenderer} - View renderer for this endpoint.
+ */
+const ViewAddEffect = function(bot) {
 	return function(req, res) {
 		if(req.query.effect && req.query.effect.trim().length > 0) {
-			bot.database.addBassEffect(req.query.effect, function(err) {
+			bot.database.addBassEffect(req.query.effect, (err) => {
 				if(err) {
 					if(err.code !== "ER_DUP_ENTRY") {
 						Winston.error("Unabled to add ne effects", err);
 					}
-					res.status(500).send({
+					res.status(HTTPCodes.invaldRequest).send({
 						okay : false,
 						reason : "internal_error"
 					});
 				}
 				else {
-					Winston.log('verbose', req.session.user.username + " added new bass-effect: \"" + req.query.effect + "\"");
-					res.status(200).send({
+					Winston.log("verbose",
+						req.session.user.username + " added new bass-effect: \"" + req.query.effect + "\""
+					);
+					res.status(HTTPCodes.okay).send({
 						okay : true
 					});
 				}
 			});
 		}
 		else {
-			res.status(400).send({
+			res.status(HTTPCodes.missingArguments).send({
 				okay : false,
 				reason : "missing_arguments"
 			})
 		}
 	}
 };
+
+export default ViewAddEffects;
