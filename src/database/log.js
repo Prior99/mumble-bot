29c750ddaf1cc8a5ffa9769463daabbdb152e015
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-17 14:27:31 +0100

    src/database/log.js and src/database/mumbleUsers.js now properly documented, using async await and lint free.

--- src/database/log.js
+++ src/database/log.js
@@ -4,14 +4,11 @@
  * @return {undefined}
  */
 const DatabaseLog = function(Database) {
-	Database.prototype.listLog = function(callback) {
-		this.pool.query("SELECT level, message, `timestamp` FROM Log ORDER BY `timestamp` DESC LIMIT 500",
-			(err, rows) => {
-				if(this._checkError(err, callback)) {
-					callback(null, rows);
-				}
-			}
+	Database.prototype.listLog = async function(callback) {
+		const rows = await this.pool.query(
+			"SELECT level, message, `timestamp` FROM Log ORDER BY `timestamp` DESC LIMIT 500"
 		);
+		return rows;
 	}
 }
 
