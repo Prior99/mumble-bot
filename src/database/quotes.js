deb1b6c5ef71457d7c16b1d5333410626865f10c
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-17 14:49:06 +0100

    src/database/quotes.js now properly documented, lint free and using async await.

--- src/database/quotes.js
+++ src/database/quotes.js
@@ -5,99 +5,87 @@
  */
 const DatabaseQuotes = function(Database) {
 	/**
-	 * Add a quote to the database.
+	 * <b>Async</b> Add a quote to the database.
 	 * @param {string} quote - Text of the quote.
 	 * @param {string} author - Author of the quote.
-	 * @param callback - Called once the query is done.
+	 * @return {number} - Unique id of the newly generated quote.
 	 */
-	Database.prototype.addQuote = function(quote, author, callback) {
+	Database.prototype.addQuote = async function(quote, author) {
 		if(quote && author) {
-			this.pool.query("INSERT INTO Quotes(quote, author, submitted) VALUES(?, ?, ?)",
-				[quote, author, new Date()], function(err, result) {
-					if(this._checkError(err, callback)) {
-						if(callback) { callback(null, result.insertId); }
-					}
-				}.bind(this)
+			const result = await this.pool.query("INSERT INTO Quotes(quote, author, submitted) VALUES(?, ?, ?)",
+				[quote, author, new Date()]
 			);
+			return result.insertId;
 		}
 		else {
-			var err = new Error("Not all needed arguments were supplied.");
-			if(callback) { callback(err); }
-			else { throw err; }
+			throw new Error("Not all needed arguments were supplied.");
 		}
 	};
 
 	/**
-	* Returns a random quote from the database.
-	* @param callback - Called once the query is done.
-	*/
-	Database.prototype.getRandomQuote = function(callback) {
-		this.pool.query("SELECT quote, author, submitted, used, id FROM Quotes, (SELECT RAND() * (SELECT MAX(id) FROM Quotes) AS tid) AS Tmp WHERE Quotes.id >= Tmp.tid ORDER BY id ASC LIMIT 1",
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					this.increaseQuoteUsage(rows[0].id);
-					if(callback) { callback(null, rows[0]); }
-				}
-			}.bind(this)
-		);
-	};
+	 * A quote in the database.
+	 * @typedef Quote
+	 * @property {string} quote The content of the quote.
+	 * @property {string} author - The person from which the quote originated.
+	 * @property {date} submitted - The date and time the quote was submitted.
+	 * @property {number} used - How often the quote was already played back.
+	 * @property {number} id - The unique id of this quote.
+	 */
 	/**
-	* Looks up the details about a specified quote.
-	* @param {number} quote - Quote to look up.
-	* @param callback - Called once the query is done.
-	*/
-	Database.prototype.getQuote = function(id, callback) {
-		this.pool.query("SELECT quote, author, submitted, used FROM Quotes WHERE id = ?", [id],
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					if(rows.length >= 1) {
-						this.increaseQuoteUsage(id);
-						if(callback) { callback(null, rows[0]); }
-					}
-					else {
-						var err = new Error("Unknown quote");
-						if(callback) { callback(err); }
-						else throw err;
-					}
-				}
-			}.bind(this)
+	 * <b>Async</b> Returns a random quote from the database.
+	 * @return {Quote} - The randomly selected quote.
+	 */
+	Database.prototype.getRandomQuote = async function() {
+		const rows = await this.pool.query(
+			"SELECT quote, author, submitted, used, id FROM Quotes " +
+			"(SELECT RAND() * (SELECT MAX(id) FROM Quotes) AS tid) AS Tmp " +
+			"WHERE Quotes.id >= Tmp.tid ORDER BY id ASC LIMIT 1"
 		);
+		this.increaseQuoteUsage(rows[0].id);
+		return rows[0];
 	};
 
 	/**
-	 * Increase the amount of time this quote has already been played back..
+	 * <b>Async</b> Looks up the details about a specified quote.
+	 * @param {number} id - Unique id of the quote to look up.
+	 * @return {Quote} - The quote identified by the given id.
+	 */
+	Database.prototype.getQuote = function(id) {
+		const rows = this.pool.query("SELECT quote, author, submitted, used FROM Quotes WHERE id = ?", [id]);
+		if(rows.length >= 1) {
+			this.increaseQuoteUsage(id);
+			return rows[0];
+		}
+		else {
+			throw new Error("Unknown quote");
+		}
+	};
+
+	/**
+	 * <b>Async</b> Increase the amount of time this quote has already been played back..
 	 * @param {number} id - Id of the quote to increase the amount of.
+	 * @return {undefined}
 	 */
-	Database.prototype.increaseQuoteUsage = function(id) {
-		this.pool.query("UPDATE Quotes SET used = used + 1 WHERE id = ?", [id]);
+	Database.prototype.increaseQuoteUsage = async function(id) {
+		await this.pool.query("UPDATE Quotes SET used = used + 1 WHERE id = ?", [id]);
 	};
 
 	/**
-	 * Retrieves the total amount of quotes in the database.
-	 * @param callback - Called after the amount was retrieved.
+	 * <b>Async</b> Retrieves the total amount of quotes in the database.
+	 * @return {number} - How many quotes are stored in the database.
 	 */
-	Database.prototype.getQuoteCount = function(callback) {
-		this.pool.query("SELECT COUNT(id) AS amount FROM Quotes",
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					if(callback) { callback(null, rows[0].amount); }
-				}
-			}.bind(this)
-		);
+	Database.prototype.getQuoteCount = async function() {
+		const rows = await this.pool.query("SELECT COUNT(id) AS amount FROM Quotes");
+		return rows[0].amount;
 	};
 
 	/**
-	 * Retrieve a list with all quotes.
-	 * @param callback - Called after the query was done.
+	 * <b>Async</b> Retrieve a list with all quotes.
+	 * @return {Quote[]} - A list with all quotes.
 	 */
-	Database.prototype.getQuoteList = function(callback) {
-		this.pool.query("SELECT id, author, quote, submitted, used FROM Quotes",
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					if(callback) { callback(null, rows); }
-				}
-			}.bind(this)
-		);
+	Database.prototype.getQuoteList = function() {
+		const rows = this.pool.query("SELECT id, author, quote, submitted, used FROM Quotes");
+		return rows;
 	};
 };
 
