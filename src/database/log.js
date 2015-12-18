06c2c8b4aa797fc77d107dbcf10d4daba1d78c58
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-19 00:40:28 +0100

    Added missing doc to src/database/log.js.

--- src/database/log.js
+++ src/database/log.js
@@ -4,7 +4,18 @@
  * @return {undefined}
  */
 const DatabaseLog = function(Database) {
-	Database.prototype.listLog = async function(callback) {
+	/**
+	 * One entry in the bots log.
+	 * @typedef LogEntry
+	 * @property {string} level - Loglevel (Consult winston for this.) error, warning, verbose, info, etc...
+	 * @property {string} message - The message that was logged.
+	 * @property {date} timestamp - The date and time this was logged.
+	 */
+	/**
+	 * <b>Async</b> List the last 500 entries of the servers log.
+	 * @return {LogEntry[]} - Last 500 log entries.
+	 */
+	Database.prototype.listLog = async function() {
 		const rows = await this.pool.query(
 			"SELECT level, message, `timestamp` FROM Log ORDER BY `timestamp` DESC LIMIT 500"
 		);
