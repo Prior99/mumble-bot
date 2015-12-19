dc1265672f90b960901281443f671a94d2918d54
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-19 11:39:27 +0100

    Doc, async-await and lint-fixes for src/database/rss.js and src/database/settings.js.

--- src/database/rss.js
+++ src/database/rss.js
@@ -4,50 +4,58 @@
  * @return {undefined}
  */
 const DatabaseRSS = function(Database) {
-	Database.prototype.addRSSFeed = function(url, name, callback) {
-		this.pool.query("INSERT INTO RSS(url, name) VALUES (?, ?)", [url, name],
-			function(err, result) {
-				if(this._checkError(err, callback)) {
-					if(callback) { callback(null); }
-				}
-			}.bind(this)
-		);
+	/**
+	 * @typedef RSSFeed
+	 * @property {string} url - URL to the RSS feed.
+	 * @property {string} name - Human readable name of the feed.
+	 * @property {number} id - Unique numerical id for the feed.
+	 */
+	/**
+	 * <b>Async</b> Add a new RSS feed that should be crawled.
+	 * @param {string} url - URL of the feed to subscribe to.
+	 * @param {string} name - Name of the feed.
+	 * @return {undefined}
+	 */
+	Database.prototype.addRSSFeed = async function(url, name) {
+		await this.pool.query("INSERT INTO RSS(url, name) VALUES (?, ?)", [url, name]);
 	};
-	Database.prototype.removeRSSFeed = function(id, callback) {
-		this.pool.query("DELETE FROM RSS WHERE id = ?", [id],
-			function(err, result) {
-				if(this._checkError(err, callback)) {
-					if(callback) { callback(null); }
-				}
-			}.bind(this)
-		);
+
+	/**
+	 * <b>Async</b> Remove a RSS feed to be crawled from the pool of RSS feeds to crawl.
+	 * @param {number} id - Unique id of the feed to unsubscribe.
+	 * @return {undefined}
+	 */
+	Database.prototype.removeRSSFeed = async function(id) {
+		await this.pool.query("DELETE FROM RSS WHERE id = ?", [id]);
 	};
-	Database.prototype.listRSSFeeds = function(callback) {
-		this.pool.query("SELECT id, url, name FROM RSS",
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					callback(null, rows);
-				}
-			}.bind(this)
-		);
+
+	/**
+	 * <b>Async</b> Get a list of all subscribed RSS feeds.
+	 * @return {RSSFeed[]} - List of all subscribed RSS feeds.
+	 */
+	Database.prototype.listRSSFeeds = async function() {
+		const rows = await this.pool.query("SELECT id, url, name FROM RSS");
+		return rows;
 	};
-	Database.prototype.isRSSFeedEntryKnown = function(hash, callback) {
-		this.pool.query("SELECT hash FROM KnownRSSEntries WHERE hash = ?", [hash],
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					callback(null, rows.length > 0);
-				}
-			}.bind(this)
-		);
+
+	/**
+	 * <b>Async</b> Check whether an article is already known.
+	 * @param {string} hash - Unique hash of the article.
+	 * @return {boolean} - Whether the article is already known.
+	 */
+	Database.prototype.isRSSFeedEntryKnown = async function(hash) {
+		const rows = await this.pool.query("SELECT hash FROM KnownRSSEntries WHERE hash = ?", [hash]);
+		return rows && rows.length > 0;
 	};
-	Database.prototype.addKnownRSSFeedEntry = function(hash, url, callback) {
-		this.pool.query("INSERT INTO KnownRSSEntries(hash, url, seen) VALUES(?, ?, ?)", [hash, url, new Date()],
-			function(err, rows) {
-				if(this._checkError(err, callback)) {
-					if(callback) { callback(null); }
-				}
-			}.bind(this)
-		);
+
+	/**
+	 * <b>Async</b> Add an article to the list of already known RSS articles.
+	 * @param {string} hash - Unique hash of the article.
+	 * @param {string} url - URL of the article.
+	 * @return {undefined}
+	 */
+	Database.prototype.addKnownRSSFeedEntry = async function(hash, url) {
+		await this.pool.query("INSERT INTO KnownRSSEntries(hash, url, seen) VALUES(?, ?, ?)", [hash, url, new Date()]);
 	};
 };
 
--- src/database/settings.js
+++ src/database/settings.js
@@ -4,35 +4,49 @@
  * @return {undefined}
  */
 const DatabaseSettings = function(Database) {
-	Database.prototype.getSetting = function(user, setting, callback) {
-		this.pool.query("SELECT value FROM UserSettings WHERE user = ? AND setting = ?", [user.id, setting], function(err, rows) {
-			if(this._checkError(err, callback)) {
-				if(rows.length > 0) {
-					if(callback) { callback(null, rows[0].value); }
-				}
-				else {
-					if(callback) { callback(null, null); }
-				}
-			}
-		}.bind(this));
+	/**
+	 * <b>Async</b> Get get the specified setting from to the given user.
+	 * @param {DatabaseUser} user - User to fetch the setting from.
+	 * @param {string} setting - Settings key to fetch the value from.
+	 * @return {string} - The value in its unchanged string representation as taken from the database.
+	 */
+	Database.prototype.getSetting = async function(user, setting) {
+		const rows = await this.pool.query("SELECT value FROM UserSettings WHERE user = ? AND setting = ?",
+			[user.id, setting]
+		);
+		if(rows && rows.length > 0) {
+			return rows[0].value;
+		}
+		else {
+			return null;
+		}
 	};
 
-	Database.prototype.getSettings = function(user, callback) {
-		this.pool.query("SELECT setting, value FROM UserSettings WHERE user = ?", [user.id], function(err, rows) {
-			if(this._checkError(err, callback)) {
-				var settings = {};
-				rows.forEach(function(row) {
-					settings[row.setting] = JSON.parse(row.value);
-				});
-				if(callback) { callback(null, settings); }
-			}
-		}.bind(this));
+	/**
+	 * <b>Async</b> Get all settings of a specified user as an object.
+	 * @param {DatabaseUser} user - The user to get the settings from.
+	 * @return {object} - An object with each settings key as key and the corresponding value (parsed from json) as value.
+	 */
+	Database.prototype.getSettings = async function(user) {
+		const rows = await this.pool.query("SELECT setting, value FROM UserSettings WHERE user = ?", [user.id]);
+		const settings = {};
+		rows.forEach((row) => settings[row.setting] = JSON.parse(row.value));
+		return settings;
 	};
 
-	Database.prototype.setSetting = function(user, setting, value, callback) {
-		this.pool.query("INSERT INTO UserSettings(user, setting, value) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE value = VALUES(value)", [user.id, setting, value], function(err) {
-			if(this._checkError(err, callback) && callback) { callback(); };
-		}.bind(this));
+	/**
+	 * <b>Async</b> Set a specified setting to the specified value.
+	 * @param {DatabaseUser} user - User to change the settings of.
+	 * @param {string} setting - The setting to change.
+	 * @param {string} value - The value as already serialized string to store in the database.
+	 * @return {undefined}
+	 */
+	Database.prototype.setSetting = async function(user, setting, value) {
+		await this.pool.query(
+			"INSERT INTO UserSettings(user, setting, value) " +
+			"VALUES(?, ?, ?) " +
+			"ON DUPLICATE KEY UPDATE value = VALUES(value)", [user.id, setting, value]
+		);
 	};
 };
 
