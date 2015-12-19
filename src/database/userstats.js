ea0553d669b387ab9dcaaed7e5be191138d5977a
Frederick Gnodtke <frederick@gnodtke.net>
2015-12-19 13:40:00 +0100

    src/database/userstats.js now lint free, using async await and properly documented.

--- src/database/userstats.js
+++ src/database/userstats.js
@@ -1,85 +1,118 @@
-var Winston = require("winston");
-var Promise = require("promise");
+import * as Winston from "winston";
 /**
  * Extends the database with methods for statistics.
  * @param {Database} Database - The Database class to extend.
  * @return {undefined}
  */
 const DatabaseUserStats = function(Database) {
-	Database.prototype.writeUserStatsSpeak = function(user, started, ended, callback) {
-		Promise.denodeify(this.pool.query.bind(this.pool))("INSERT INTO UserStatsSpeaking(user, started, ended) VALUES(?, ?, ?)", [user.id, started, ended])
-		.catch(callback)
-		.then(function() {
-			if(callback) {
-				callback();
-			}
-		});
+	const millisecondsPerSecond = 1000;
+	/**
+	 * <b>Async</b> Write a new set of statistics into the database when a user has spoken.
+	 * @param {DatabaseUser} user - User that has spoken.
+	 * @param {date} started - When the speech has started.
+	 * @param {date} ended - When the speech has ended.
+	 * @return {undefined}
+	 */
+	Database.prototype.writeUserStatsSpeak = async function(user, started, ended) {
+		await this.pool.query(
+			"INSERT INTO UserStatsSpeaking(user, started, ended) VALUES(?, ?, ?)", [user.id, started, ended]
+		);
 	};
-	Database.prototype.writeUserStatsOnline = function(user, started, ended, callback) {
-		Promise.denodeify(this.pool.query.bind(this.pool))("INSERT INTO UserStatsOnline(user, started, ended) VALUES(?, ?, ?)", [user.id, started, ended])
-		.catch(callback)
-		.then(function() {
-			if(callback) {
-				callback();
-			}
-		});
-	};
-	Database.prototype.getSpokenPerHour = function(callback) {
-		Promise.denodeify(this.pool.query.bind(this.pool))("SELECT HOUR(started) AS hour, SUM(TIME_TO_SEC(ended-started)) AS amount FROM UserStatsSpeaking GROUP BY HOUR(started)")
-		.catch(callback)
-		.then(function(rows) {
-			if(callback) {
-				callback(null, rows.map(function(elem) {
-					return {
-						hour : elem.hour,
-						amount : new Date(elem.amount * 1000)
-					}
-				}));
-			}
-		});
 
+	/**
+	 * <b>Async</b> Write a new set of statistics into the database when a user is online.
+	 * @param {DatabaseUser} user - User that has spoken.
+	 * @param {date} started - When the user got online.
+	 * @param {date} ended - When the user got offline.
+	 * @return {undefined}
+	 */
+	Database.prototype.writeUserStatsOnline = async function(user, started, ended) {
+		await this.pool.query(
+			"INSERT INTO UserStatsOnline(user, started, ended) VALUES(?, ?, ?)", [user.id, started, ended]
+		);
 	};
-	Database.prototype.getSpokenPerUser = function(callback) {
-		Promise.denodeify(this.pool.query.bind(this.pool))("SELECT username AS user, SUM(TIME_TO_SEC(ended-started)) AS amount FROM UserStatsSpeaking LEFT JOIN Users u ON user = u.id GROUP BY user")
-		.catch(callback)
-		.then(function(rows) {
-			if(callback) {
-				callback(null, rows.map(function(elem) {
-					return {
-						user : elem.user,
-						amount : new Date(elem.amount * 1000)
-					}
-				}));
-			}
-		});
+
+	/**
+	 * @typedef StatObjectSpeechPerHour
+	 * @property {number} hour - The hour this object is representing.
+	 * @property {number} amount - Amount of speech in this hour.
+	 */
+	/**
+	 * <b>Async</b> Get statistics about how much speech per hour was produced.
+	 * @return {StatObjectSpeechPerHour[]} - List of objects representing the statistics requested.
+	 */
+	Database.prototype.getSpokenPerHour = async function() {
+		const rows = await this.pool.query(
+			"SELECT HOUR(started) AS hour, SUM(TIME_TO_SEC(ended-started)) AS amount " +
+			"FROM UserStatsSpeaking GROUP BY HOUR(started)"
+		);
+		return rows.map((elem) => ({
+			hour : elem.hour,
+			amount : new Date(elem.amount * millisecondsPerSecond)
+		}));
 	};
-	Database.prototype.getSpokenPerWeekday = function(callback) {
-		Promise.denodeify(this.pool.query.bind(this.pool))("SELECT DAYOFWEEK(started) AS day, SUM(TIME_TO_SEC(ended-started)) AS amount FROM UserStatsSpeaking GROUP BY DAYOFWEEK(started)")
-		.catch(callback)
-		.then(function(rows) {
-			if(callback) {
-				callback(null, rows.map(function(elem) {
-					return {
-						day : elem.day,
-						amount : new Date(elem.amount * 1000)
-					}
-				}));
-			}
-		});
+
+		/**
+		 * @typedef StatObjectSpeechPerUser
+		 * @property {string} user - Name of the user this object is representing.
+		 * @property {number} amount - Amount of speech in this hour.
+		 */
+	/**
+	 * <b>Async</b> Get statistics about the speech per user.
+	 * @return {StatObjectSpeechPerUser[]} - List of objects representing the statistics requested.
+	 */
+	Database.prototype.getSpokenPerUser = async function() {
+		const rows = await this.pool.query(
+			"SELECT username AS user, SUM(TIME_TO_SEC(ended-started)) AS amount " +
+			"FROM UserStatsSpeaking " +
+			"LEFT JOIN Users u ON user = u.id " +
+			"GROUP BY user");
+		return rows.map(elem => ({
+			user : elem.user,
+			amount : new Date(elem.amount * millisecondsPerSecond)
+		}));
 	};
-	Database.prototype.getOnlinePerUser = function(callback) {
-		Promise.denodeify(this.pool.query.bind(this.pool))("SELECT username AS user, SUM(TIME_TO_SEC(ended-started)) AS amount FROM UserStatsOnline LEFT JOIN Users u ON user = u.id GROUP BY user")
-		.catch(callback)
-		.then(function(rows) {
-			if(callback) {
-				callback(null, rows.map(function(elem) {
-					return {
-						user : elem.user,
-						amount : new Date(elem.amount * 1000)
-					}
-				}));
-			}
-		});
+
+	/**
+	 * @typedef StatObjectSpeechPerWeekday
+	 * @property {string} user - Name of the user this object is representing.
+	 * @property {number} amount - Amount of speech in this hour.
+	 */
+	/**
+	 * <b>Async</b> Get statistics about speech per weekday.
+	 * @return {StatObjectSpeechPerWeekday[]} - List of objects representing the statistics requested.
+	 */
+	Database.prototype.getSpokenPerWeekday = async function() {
+		const rows = await Promise.denodeify(this.pool.query.bind(this.pool))(
+			"SELECT DAYOFWEEK(started) AS day, SUM(TIME_TO_SEC(ended-started)) AS amount " +
+			"FROM UserStatsSpeaking " +
+			"GROUP BY DAYOFWEEK(started)"
+		);
+		return rows.map(elem => ({
+			day : elem.day,
+			amount : new Date(elem.amount * millisecondsPerSecond)
+		}));
+	};
+	/**
+	 * @typedef StatObjectOnlinePerUser
+	 * @property {string} user - Name of the user this object is representing.
+	 * @property {number} amount - Amount of time the user was online in seconds.
+	 */
+	/**
+	 * <b>Async</b> Get statistics about the online time per user.
+	 * @return {StatObjectOnlinePerUser[]} - List of objects representing the statistics requested.
+	 */
+	Database.prototype.getOnlinePerUser = async function() {
+		const rows = await Promise.denodeify(this.pool.query.bind(this.pool))(
+			"SELECT username AS user, SUM(TIME_TO_SEC(ended-started)) AS amount " +
+			"FROM UserStatsOnline " +
+			"LEFT JOIN Users u ON user = u.id " +
+			"GROUP BY user"
+		);
+		return rows.map(elem => ({
+			user : elem.user,
+			amount : new Date(elem.amount * millisecondsPerSecond)
+		}));
 	};
 };
 
