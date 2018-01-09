e9cf0fa3a9fb2a247efe9dd93e6328a33ef1a6ea
Frederick Gnodtke <frederick@gnodtke.net>
2018-01-09 16:10:21 +0100

    WIP: Move sound routes to controller

--- src/controllers/sounds.ts
+++ src/controllers/sounds.ts
@@ -1,20 +1,38 @@
-import { body, controller, route, param, is, uuid, ok } from "hyrest";
+import { context, body, controller, route, param, is, uuid, ok } from "hyrest";
 import { component, inject } from "tsdi";
 import { Connection } from "typeorm";
+import { verbose } from "winston";
 
 import { DatabaseSound } from "../models";
+import { Context } from "../context";
+import { Bot } from "..";
 
 @controller @component
 export class Sounds {
     @inject private db: Connection;
+    @inject private bot: Bot;
 
     @route("GET", "/sounds")
     public async listSounds(): Promise<DatabaseSound[]> {
-        return ok([]);
+        const sounds = await this.db.getRepository(DatabaseSound).find();
+        return ok(sounds);
     }
 
     @route("POST", "/sound/:id/play")
-    public async saveCached(@param("id") @is().validate(uuid) id: string): Promise<{}> {
+    public async playSound(@param("id") @is().validate(uuid) id: string, @context ctx?: Context): Promise<{}> {
+        const sound = await this.db.getRepository(DatabaseSound).findOneById(id);
+        sound.used++;
+        await this.db.getRepository(DatabaseSound).save(sound);
+
+        const currentUser = await ctx.currentUser();
+        this.bot.playSound(`${this.bot.options.paths.uploaded}/${id}`, {
+            type: "sound",
+            sound,
+            user: currentUser
+        });
+
+        verbose(`${currentUser.username} played sound #${id}`);
+
         return ok();
     }
 }
