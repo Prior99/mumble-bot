6ff29612302fb5f53e319bf5a0ee1eea544e796b
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-25 18:10:57 +0200

    Add dashboard page

--- src/web/pages/dashboard/dashboard.tsx
+++ src/web/pages/dashboard/dashboard.tsx
@@ -3,11 +3,9 @@ import { observer } from "mobx-react";
 import { external, inject } from "tsdi";
 import { observable } from "mobx";
 import { bind } from "decko";
-import { Menu } from "semantic-ui-react";
 
 import { requireLogin, LoginStore } from "../../../common-ui";
 import { Content } from "../../components";
-import * as css from "./dashboard.scss";
 
 @requireLogin @observer @external
 export class PageDashboard extends React.Component {
@@ -16,14 +14,7 @@ export class PageDashboard extends React.Component {
     public render() {
         return (
             <Content>
-                <div className={css.container}>
-                    <div className={css.feed}>
-                        <Menu tabular>
-                        </Menu>
-                    </div>
-                    <div className={css.side}>
-                    </div>
-                </div>
+                <h1>Dashboard</h1>
             </Content>
         );
     }
